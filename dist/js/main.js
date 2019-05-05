011aac095a1d389463ed22e6d8637cc7340a23d8
Carlton Joseph <carlton.joseph@gmail.com>
2019-05-05 13:29:25 -0700

    add menu icon

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,24 @@
-console.log("hi");
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+let showMenu = false;
+
+const toggleMenu = () => {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+  }
+  showMenu = !showMenu;
+  console.log(showMenu);
+};
+
+menuBtn.addEventListener("click", toggleMenu);
